c3ee52c521456da8ca0354f17a6744bd0f5902d0
Diego Restrepo <rescolo@gmail.com>
2015-08-25 17:43:53 -0500

    Includeing web page

--- fix_index.php.sh
+++ fix_index.php.sh
@@ -1,7 +1,14 @@
+if [ "$1" ];then
+    share="$1"
+else
+   share=$(git log | grep share: | awk -F":" '{print $2}' | head -n1)
+fi
+cat <<EOF > index.php
 <center>
 <h1>Nuevos Caminos por Antioquia</h1>
 Haga click sobre el icono <img src="iconob.png" alt="Abrir in wikiloc" height="30"> para abrir la ruta en <a href="http://www.wikiloc.com/">wikiloc</a> (<a href="http://geojson.io/#id=github:rescolo/ncxa/blob/master/NCXA.geojson&map=10/6.1610/-75.1808"> Edit<a/>) [<a href="https://www.google.com/maps/d/embed?mid=z2NCe4lIOjVA.kgQgTDLLMHfw">Google Maps</a>]
 <!-- <iframe src="https://www.google.com/maps/d/u/0/embed?mid=z2NCe4lIOjVA.kgQgTDLLMHfw" width="80%" height="90%"></iframe> -->
-<iframe frameborder="0" width="100%" height="85%" src="http://bl.ocks.org/d/14e0c8c79d38117e16ba"></iframe> <br/>
+<iframe frameborder="0" width="100%" height="85%" src="http://bl.ocks.org/d/$share"></iframe> <br/>
 <small>&#169; <a href="https://www.facebook.com/NCXAntioquia">NCX Antioquia</a>
 </center>
+EOF
