08cdede5151f961da0d6b81a699c4c1154ed6c8e
Diego Restrepo <rescolo@gmail.com>
2015-08-26 14:28:52 -0500

    Fix generate full kml script

--- kml/000_generate_full_kml.sh
+++ kml/000_generate_full_kml.sh
@@ -1,1 +1,23 @@
-./000_catkml.sh $(ls -l *.kml |awk '{print $9}' | grep -E '^[0-9][0-9][0-9]') > ../map_all.kml
+fullist=$(ls -l *.kml |awk '{print $9}' | grep -E '^[0-9][0-9][0-9]')
+if [ ! "$1" ];then
+    echo USAGE $0 istart
+    exit
+fi
+istart=$1
+i=0
+
+for f in $fullist;do
+ if [ $i -lt $(($istart-1)) ]; then   
+   i=$(($i+1))
+   if [ $i -lt 10 ];then
+       fullist=$(echo "$fullist" | grep -v '^00'$i'_')
+   elif  [ $i -ge 10 ] && [ $i -lt 100 ];then
+       fullist=$(echo "$fullist" | grep -v '^0'$i'_')
+   else
+       fullist=$(echo "$fullist" | grep -v '^'$i'_')       
+   fi
+ fi
+done
+>&2 echo processing...
+>&2 echo $fullist
+./000_catkml.sh "$fullist" > ../map_all.kml
