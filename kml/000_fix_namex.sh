40285c11a53b3302d31c664160da4b361d38c298
Diego Restrepo <rescolo@gmail.com>
2015-08-26 06:13:54 -0500

    Script

---
+++ kml/000_fix_namex.sh
@@ -0,0 +1,19 @@
+if [ ! "$1" ];then
+    echo USAGE $0 i
+    exit
+fi
+files=$(ls -lt | awk '{print $9}' | grep -E '^[a-zA-Z]')
+i=$1
+for f in $files;do
+   i=$(($i+1))
+   if [ $i -lt 10 ];then
+       p=00$i
+   elif [ $i -lt 100 ];then
+       p=0$i
+   else
+       p=$i
+   fi
+   git mv $f ${p}_$f
+done
+
+
