fc64136afb81ec0f1a407d478e0697aa7d30ab34
Diego Restrepo <rescolo@gmail.com>
2015-08-27 10:50:02 -0500

    CartoDB link

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <center>
 <h1>Nuevos Caminos por Antioquia</h1>
-Haga click sobre el icono <img src="iconob.png" alt="Abrir in wikiloc" height="30"> para abrir la ruta en <a href="http://www.wikiloc.com/">wikiloc</a> (<a href="http://geojson.io/#id=github:rescolo/ncxa/blob/master/NCXA.geojson&map=10/6.1610/-75.1808"> Edit<a/>) [<a href="https://www.google.com/maps/d/embed?mid=z2NCe4lIOjVA.kgQgTDLLMHfw">Google Maps</a>]
+Haga click sobre el icono <img src="iconob.png" alt="Abrir in wikiloc" height="30"> para abrir la ruta en <a href="http://www.wikiloc.com/">wikiloc</a> (<a href="http://geojson.io/#id=github:rescolo/ncxa/blob/master/NCXA.geojson&map=10/6.1610/-75.1808"> Edit<a/>) [<a href="https://www.google.com/maps/d/embed?mid=z2NCe4lIOjVA.kgQgTDLLMHfw">Google Maps</a>] [<a href="https://diegorestrepo.cartodb.com/viz/b142ee1c-4cd2-11e5-8c4e-0e8dde98a187/public_map">CartoDB</a>]
 <!-- <iframe src="https://www.google.com/maps/d/u/0/embed?mid=z2NCe4lIOjVA.kgQgTDLLMHfw" width="80%" height="90%"></iframe> -->
 <!--<iframe frameborder="0" width="100%" height="85%" src="http://bl.ocks.org/d/48aed2e6bb0852fafac9"></iframe> -->
 <iframe frameborder="0" width="100%" height="85%" src="http://gfif.udea.edu.co/ncxa/ncxa.html"></iframe><br/>
