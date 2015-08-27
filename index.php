fbff203301f1c406e70cfdf236a50420772d0209
Diego Restrepo <rescolo@gmail.com>
2015-08-27 10:36:45 -0500

    Switching to mapbox

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <h1>Nuevos Caminos por Antioquia</h1>
 Haga click sobre el icono <img src="iconob.png" alt="Abrir in wikiloc" height="30"> para abrir la ruta en <a href="http://www.wikiloc.com/">wikiloc</a> (<a href="http://geojson.io/#id=github:rescolo/ncxa/blob/master/NCXA.geojson&map=10/6.1610/-75.1808"> Edit<a/>) [<a href="https://www.google.com/maps/d/embed?mid=z2NCe4lIOjVA.kgQgTDLLMHfw">Google Maps</a>]
 <!-- <iframe src="https://www.google.com/maps/d/u/0/embed?mid=z2NCe4lIOjVA.kgQgTDLLMHfw" width="80%" height="90%"></iframe> -->
-<iframe frameborder="0" width="100%" height="85%" src="http://bl.ocks.org/d/48aed2e6bb0852fafac9"></iframe> <br/>
+<!--<iframe frameborder="0" width="100%" height="85%" src="http://bl.ocks.org/d/48aed2e6bb0852fafac9"></iframe> -->
+<iframe frameborder="0" width="100%" height="85%" src="http://gfif.udea.edu.co/ncxa/ncxa.html"></iframe><br/>
 <small>&#169; <a href="https://www.facebook.com/NCXAntioquia">NCX Antioquia</a>
 </center>
