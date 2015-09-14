c823d88e6d8fd403bac2ef08d9f732bd03220b8e
Diego Restrepo <rescolo@gmail.com>
2015-09-14 18:49:32 -0500

    Last track

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <center>
 <h3>Nuevos Caminos por Antioquia</h3>
 Haga click sobre el camino para abrir la ruta en <a href="http://www.wikiloc.com/">wikiloc</a> y sobre el icono <img src="icono.png" alt="Abrir in wikiloc" height="15"> para ver detalles.
-<div id='Distance'>Total recorrido: 12813 km.</div> <div id='Desnivel'>Desnivel total: 153342 metros</div> (<a href="http://geojson.io/#id=github:rescolo/ncxa/blob/master/NCXA_201_225.geojson&map=10/6.1610/-75.1808"> Edit<a/>) [<a href="https://www.google.com/maps/d/embed?mid=z2NCe4lIOjVA.kgQgTDLLMHfw">Google Maps</a>] [<a href="https://diegorestrepo.cartodb.com/viz/b142ee1c-4cd2-11e5-8c4e-0e8dde98a187/public_map">CartoDB</a>] [<a href="http://gfif.udea.edu.co/ncxa/ncxa.html">Fullscreen</a>] [<a href="https://github.com/rescolo/ncxa">GitHub</a>]
+<div id='Distance'>Total recorrido: 12867 km.</div> <div id='Desnivel'>Desnivel total: 154661 metros</div> (<a href="http://geojson.io/#id=github:rescolo/ncxa/blob/master/NCXA_201_225.geojson&map=10/6.1610/-75.1808"> Edit<a/>) [<a href="https://www.google.com/maps/d/embed?mid=z2NCe4lIOjVA.kgQgTDLLMHfw">Google Maps</a>] [<a href="https://diegorestrepo.cartodb.com/viz/b142ee1c-4cd2-11e5-8c4e-0e8dde98a187/public_map">CartoDB</a>] [<a href="http://gfif.udea.edu.co/ncxa/ncxa.html">Fullscreen</a>] [<a href="https://github.com/rescolo/ncxa">GitHub</a>]
 <!-- <iframe src="https://www.google.com/maps/d/u/0/embed?mid=z2NCe4lIOjVA.kgQgTDLLMHfw" width="80%" height="90%"></iframe> -->
 <!--<iframe frameborder="0" width="100%" height="85%" src="http://bl.ocks.org/d/48aed2e6bb0852fafac9"></iframe> -->
 <iframe frameborder="0" width="100%" height="85%" src="http://gfif.udea.edu.co/ncxa/ncxa.html"></iframe><br/>
